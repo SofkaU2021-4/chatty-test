127fdc834e070d0a5082db6ab9c74f2944026139
iSebastianCabrera <jscrojas25@yahoo.es>
2022-01-02 12:02:19 -0500

    testing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="pt-5">
+      <div className="container text-center">
+        <p>&copy; Chatty 2022 Sofka.</p>
+        <p>El Sebas .</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
